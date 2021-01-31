056977e55ea5ceca00a4e0188691add7fc40fd2f
geninhocell <geninhocell@gmail.com>
2021-01-31 02:04:08 -0300

    31-01-2021-02-04

--- modulo-4/gobarber-backend/src/config/auth.ts
+++ modulo-4/gobarber-backend/src/config/auth.ts
@@ -1,6 +1,13 @@
+interface IJwtConfig {
+  jwt: {
+    secret: string;
+    expiresIn: string;
+  };
+}
+
 export default {
   jwt: {
     secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
-};
+} as IJwtConfig;
