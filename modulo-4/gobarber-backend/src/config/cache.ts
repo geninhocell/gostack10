4cca50a92a783112ab550a3acc1e7b68cfcf713b
geninhocell <geninhocell@gmail.com>
2021-02-01 11:39:16 -0300

    01-02-2021-11-39

--- modulo-4/gobarber-backend/src/config/cache.ts
+++ modulo-4/gobarber-backend/src/config/cache.ts
@@ -15,7 +15,7 @@ export default {
     redis: {
       host: process.env.REDIS_HOST || 'localhost',
       port: process.env.REDIS_PORT || 6379,
-      password: process.env.REDIS_PASSWORD,
+      password: process.env.REDIS_PASSWORD || undefined,
     },
   },
 } as ICacheConfig;
